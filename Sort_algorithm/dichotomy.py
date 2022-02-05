87143a760369e1224bc93c4d09ec749237ef21d4
KKKKSHI30 <ks1837@georgetown.edu>
2022-02-05 09:06:42 -0500

    Sort_Algorithm

--- Sort_algorithm/dichotomy.py
+++ Sort_algorithm/dichotomy.py
@@ -1,19 +1,34 @@
 # Ke Shi on Feb 5th, 2022
 # Using Dichotomy to find a number if it existed in an ordered array
+# Note: lo = half + 1 to prevent the lst[half] == value situation
+# hi = half - for the similar case
 
 # Loop
-def binary_search_while(lst, value):
+def BinarySearchWhile(lst, value):
     lo, hi = 0, len(lst) - 1
-    if value > lst[len(lst)-1] or value < lst[0]:
-        return print("not in it")
     while lo <= hi:
-        half = int((lo + hi) / 2)
+        half = ((lo + hi) // 2)
         if lst[half] > value:
             hi = half - 1
-        elif lst[half] < value:
-            lo = half - 1
+        elif value > lst[half]:
+            lo = half + 1
         elif lst[half] == value:
             return print("in it")
     return print("not in it")
 
 A = [1,2,3,4,5]
+
+# Recursion
+
+def BinarySearchRecursion(lst, value,lo,hi):
+    if lst[lo] <= value <= lst[hi]:
+        half = ((lo + hi) // 2)
+        if lst[half] == value:
+            return print("in it")
+        elif lst[half] < value:
+            return BinarySearchRecursion(lst,value,half+1,hi)
+        else:
+            return BinarySearchRecursion(lst,value,lo,half-1)
+    else:
+        return print("not in it")
+
