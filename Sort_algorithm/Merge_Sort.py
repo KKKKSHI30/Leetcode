7b1c646352ebb165c998c4583c1035192b12bdc2
KKKKSHI30 <ks1837@georgetown.edu>
2022-02-08 09:21:06 -0500

    Merge_Sort

---
+++ Sort_algorithm/Merge_Sort.py
@@ -0,0 +1,59 @@
+# Ke Shi on Feb 8th, 2022
+def merge_sort(nums):
+    """
+    :param nums: List[int]
+    :return: List[int]
+    """
+    if len(nums) == 0 or len(nums) == 1:
+        return nums
+    process(nums,0,len(nums)-1)
+    return nums
+
+def process(nums,left,right):
+    """
+    :param nums: List[int]
+    :param left: int
+    :param right: int
+    :return: List[int]
+    """
+    if left == right:
+        return nums
+    else:
+        mid = (left + right) // 2
+        print(f"mid is {mid} rightnow.")
+        process(nums,left, mid)
+        process(nums,mid+1, right)
+        merge(nums,left,mid,right)
+    return nums
+
+def merge(nums,left,mid,right):
+    """
+    :param nums: List[int]
+    :param left: int
+    :param mid: int
+    :param right: int
+    :return: List[int]
+    """
+    lst = []
+    i,j = left, mid+1
+    for k in range(right-left+1):
+        if i == (mid+1):
+            lst.append(nums[j])
+            j += 1
+        elif j == (right + 1):
+            lst.append(nums[i])
+        elif nums[i] <= nums[j]:
+            lst.append(nums[i])
+            i += 1
+        else:
+            lst.append(nums[j])
+            j += 1
+    for k in range(len(lst)):
+        nums[left] = lst[k]
+        left += 1
+    return lst
+
+A = [38,27,43,3,9,82,10]
+merge_sort(A)
+
+
