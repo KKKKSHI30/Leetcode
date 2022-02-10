1f5b258c785348dadb18297d8dc2f715e4d3a921
KKKKSHI30 <ks1837@georgetown.edu>
2022-02-10 01:10:26 -0500

    reverse_order pair

--- Sort_algorithm/Reverse_Order_Pair.py
+++ Sort_algorithm/Reverse_Order_Pair.py
@@ -18,20 +18,11 @@ def process(nums,left,right):
     if left == right:
         return []
     mid = (left + right) //2
-    print("in process, ",nums[left:mid+1], "and ", nums[mid+1:right+1])
     nums2 = []
     nums2.extend(process(nums,left, mid))
     nums2.extend(process(nums, mid+1,right))
     nums2.extend(merge(nums,left,mid,right))
-    #return process(nums,left, mid) + process(nums, mid+1,right) + merge(nums,left,mid,right)
     return nums2
-
-"""
-A = [1,3,4,2,5]
-= [5,4,3,2,1]
-= 2 tuple
-= (3,2),(4,2)
-"""
 
 def merge(nums,left,mid,right):
     """
@@ -48,32 +39,22 @@ def merge(nums,left,mid,right):
     for k in range(right - left +1):
         if i == (mid +1):
             lst.extend(nums[j:right+1])
-            print("here")
-            print(f"lst is {lst}")
             break
         elif j == (right +1):
             lst.extend(nums[i:mid+1])
-            print("here2")
-            print(f"lst is {lst}")
             break
         elif nums[j] < nums[i]:
             lst.append(nums[i])
             total += right - j + 1
-            print("here3")
-            print(f"lst is {lst}")
             nums2.append((nums[i],nums[j]))
             i += 1
         else:
             lst.append(nums[j])
             j += 1
-            print("here4")
-            print(f"lst is {lst}")
 
     for k in range(len(lst)):
         nums[left] = lst[k]
         left += 1
-    print(f"nums is {nums} right now.")
-    print(f"nums2 is {nums2}")
     return nums2
 
 A = [1,3,4,2,5]
