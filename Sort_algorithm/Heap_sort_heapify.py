f9c6a5815bac906f745c59ad24c6c7b8ffeb881e
KKKKSHI30 <ks1837@georgetown.edu>
2022-02-18 01:22:18 -0500

    heap_sort

--- Sort_algorithm/Heap_sort_heapify.py
+++ Sort_algorithm/Heap_sort_heapify.py
@@ -1,5 +1,6 @@
-# Ke Shi on Feb 17th, 2022
-def heap_sort(nums):
+# Ke Shi on Feb 18th, 2022
+# quicker than heap_sort
+def heap_sort_heapify(nums):
     """
     :param nums: List[int]
     :return: List[int]
@@ -7,14 +8,10 @@ def heap_sort(nums):
     if (len(nums) == 0) or (len(nums) == 1) :
         return nums
     else:
-        #for i in range(len(nums)):
-        #    print(heap_insert(nums,i))
         i = len(nums)-1
         while (i >=0):
             heapify(nums,i,len(nums))
             i -=1
-
-
     heapSize = len(nums)
     nums[0],nums[heapSize-1] = nums[heapSize-1],nums[0]
     heapSize -= 1
@@ -24,16 +21,6 @@ def heap_sort(nums):
         heapSize -= 1
     return nums
 
-def heap_insert(nums,index):
-    """
-    :param nums: List[nums]
-    :param index: num
-    :return: List[nums]
-    """
-    while (nums[index] > nums[max((index-1)//2,0)]):
-        nums[index],nums[(index-1)//2] = nums[(index-1)//2],nums[index]
-        index = (index-1)//2
-    return nums
 
 def heapify(nums,index,heapSize):
     """
@@ -57,4 +44,4 @@ def heapify(nums,index,heapSize):
     return nums
 
 A = [3,5,2,7,3,4,9]
-print(heap_sort(A))
+print(heap_sort_heapify(A))
