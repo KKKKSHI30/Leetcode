b132fe2a2ca379a5b6f500015178be4819fb3f9a
KKKKSHI30 <ks1837@georgetown.edu>
2022-02-23 00:40:03 -0500

    Radix_sort

---
+++ Sort_algorithm/Radix_Sort.py
@@ -0,0 +1,36 @@
+# Ke Shi on Feb 21th, 2022
+import math
+def Radix_Sort(nums):
+    """
+    for non-negative value
+    :param nums: List[int]
+    :return: List[int]
+    """
+    if len(nums) == 0 or len(nums) == 1:
+        return nums
+    else:
+        radix_sort(nums,0,len(nums)-1,maxbits(nums))
+
+def maxbits(nums):
+    """
+    :param nums: List[int]
+    :return: List[int]
+    """
+    max_value = nums[0]
+    for i in range(len(nums)):
+        max_value = max(max_value,nums[i])
+    bit = 0
+    while (max_value != 0):
+        max_value = max_value//10
+        bit += 1
+    return bit
+
+
+def radix_sort(nums,left,right,digit):
+    """
+    :param nums: List[int]
+    :param left: int
+    :param right: int
+    :param digit: int
+    :return: List[int]
+    """
