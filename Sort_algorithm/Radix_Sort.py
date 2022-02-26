a95fb6beaf8bfccc465fc5a102e39383e83403ce
KKKKSHI30 <ks1837@georgetown.edu>
2022-02-26 00:41:45 -0500

    radix_sort

--- Sort_algorithm/Radix_Sort.py
+++ Sort_algorithm/Radix_Sort.py
@@ -8,7 +8,7 @@ def Radix_Sort(nums):
     if len(nums) == 0 or len(nums) == 1:
         return nums
     else:
-        radix_sort(nums,0,len(nums)-1,maxbits(nums))
+        return radix_sort(nums,0,len(nums)-1,maxbits(nums))
 
 def maxbits(nums):
     """
@@ -42,7 +42,30 @@ def radix_sort(nums,left,right,digit):
     :return: List[int]
     """
     radix = 10
-    i, j = 0,0
-    bucket = []
-    count = [0]*10
+    bucket = [0] * len(nums)
+    for d in range(1,digit+1):
+        count = [0] * 10
+        for i in range(left,right+1):
+            j = get_digit(nums[i],d)
+            count[j] += 1
+
+        for i in range(radix):
+            count[i] = count[i]+ count[i-1]
+
+        for i in reversed(range(left,right+1)):
+            j = get_digit(nums[i],d)
+            bucket[count[j]-1] = nums[i]
+            count[j] -= 1
+
+        for i in range(left,right+1):
+            nums[i] = bucket[i]
+    return nums
+
+A = [34,73,523,62,256,356,5]
+print(Radix_Sort(A))
+
+
+
+
+
 
