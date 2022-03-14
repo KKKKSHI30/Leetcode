01152ad7ae2c9bf76420a70379bd2a6a695aaba1
KKKKSHI30 <ks1837@georgetown.edu>
2022-03-14 01:36:53 -0400

    algorithm analysis

--- Data_Structures_And_Algoithms_in_Python_Goodrich/3.Algorithm_Analysis.py
+++ Data_Structures_And_Algoithms_in_Python_Goodrich/3.Algorithm_Analysis.py
@@ -1,2 +1,80 @@
 # Ke Shi on Mar 1st, 2022
-# 2.3 Class Definitions
+# 3.1 Experimental Studies
+
+from time import time
+start_time = time()
+# run algorithm
+end_time = time()
+elapsed = end_time - start_time
+
+def prefix_average1(S):
+    n = len(S)
+    A = [0] * n
+    for j in range(n):
+        total = 0
+        for i in range(j + 1):
+            total += S[i]
+        A[j] = total/(j+1)
+    return A
+
+lst = [1,2,3,4,5]
+
+def prefix_average2(S):
+    n = len(S)
+    A = [0] * n
+    for j in range(n):
+        A[j] = sum(S[0:j+1]) / (j+1)
+    return A
+
+def prefix_average3(S):
+    n = len(S)
+    A = [0] * n
+    total = 0
+    for j in range(n):
+        total += S[j]
+        A[j] = total / (j + 1)
+    return A
+
+def disjoint1(A,B,C):
+    for a in A:
+        for b in B:
+            for c in C:
+                if a == b == c:
+                    return False
+    return True
+
+def disjoint2(A,B,C):
+    for a in A:
+        for b in B:
+            if a == b:
+                for c in C:
+                    if a == c:
+                        return False
+    return True
+
+def unique1(S):
+    for j in range(len(S)):
+        for k in range(j+1, len(S)):
+            if S[j] == S[k]:
+                return False
+    return True
+
+def unique2(S):
+    temp = sorted(S)
+    for j in range(1,len(temp)):
+        if S[j-1] == S[j]:
+            return False
+    return True
+
+def find(S, val):
+    n = len(S)
+    j = 0
+    while j < n:
+        if S[j] == val:
+            return j
+        j += 1
+    return -1
+
+
+
+
