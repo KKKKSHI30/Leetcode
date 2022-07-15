93034687699e2e8fc3919707a2f5750412a2c4b3
KKKKSHI30 <ks1837@georgetown.edu>
2022-07-14 23:54:32 -0400

    traverse tree

---
+++ Tree/traverse.py
@@ -0,0 +1,10 @@
+# Ke Shi on July 14th, 2022
+
+class node:
+    def __init__(self, element, left = None, right = None):
+        self.element = element
+        self.left = left
+        self.right = right
+
+def traverse(head):
+    pass
