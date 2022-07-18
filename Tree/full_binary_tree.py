60c42b3588eb23cb4db8b9ce9052922c7e2fee91
KKKKSHI30 <ks1837@georgetown.edu>
2022-07-18 00:37:15 -0400

    Check if it's full_binary_tree

--- Tree/full_binary_tree.py
+++ Tree/full_binary_tree.py
@@ -7,5 +7,28 @@ class node:
         self.right = right
 
 def FBT(head):
-    pass
+    """Check whether a tree is a full binary tree"""
+    height, nodes = full_binary_tree(head)
+    return (2**height -1 == nodes)
 
+def full_binary_tree(head):
+    if head == None:
+        return (0, 0)
+    left_height, left_nodes = full_binary_tree(head.left)
+    right_height, right_nodes = full_binary_tree(head.right)
+    height = max(left_height, right_height) + 1
+    nodes = left_nodes + right_nodes + 1
+    return (height, nodes)
+
+
+
+tree1 = node(1,
+             node(2, node(4), node(5)),
+             node(3, node(6), node(7))
+             )
+tree2 = node(4,
+             node(2, None, node(3)),
+             node(6, node(5), node(7))
+             )
+FBT(tree1)
+FBT(tree2)
