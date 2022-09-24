a1245473e7fec1e1276cd32fe60d4e477fc8e60e
KKKKSHI <98501114+KKKKSHI30@users.noreply.github.com>
2022-09-24 01:01:33 -0400

    Tree

--- Amazon/Tree/Binary Tree Zigzag Level Order Traversal.py
+++ Amazon/Tree/Binary Tree Zigzag Level Order Traversal.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from queue import Queue
+
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -11,4 +14,19 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        return self.breath_first(root, 1)
+
+    def breath_first(self, root, num):
+        """Width traversal"""
+        q = Queue()
+        cur = root
+        q.put(cur)
+        while (cur != None and not q.empty()):
+            cur = q.get()
+            print(cur.element)
+            if cur.left:
+                q.put(cur.left)
+            if cur.right:
+                q.put(cur.right)
+
 
