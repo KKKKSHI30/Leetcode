248897ad46f26cad7e40c1d474770d1cccda83a8
KKKKSHI <98501114+KKKKSHI30@users.noreply.github.com>
2022-12-26 19:16:25 -0500

    Tree

--- Tree/543. Diameter of Binary Tree.py
+++ Tree/543. Diameter of Binary Tree.py
@@ -1,22 +1,32 @@
-class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        diameter = 0
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-        def longest_path(node):
-            if not node:
-                return 0
-            nonlocal diameter
-            # recursively find the longest path in
-            # both left child and right child
-            left_path = longest_path(node.left)
-            right_path = longest_path(node.right)
+class Solution(object):
+    totalmax = 0
 
-            # update the diameter if left_path plus right_path is larger
-            diameter = max(diameter, left_path + right_path)
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.maxdepth(root)
+        return self.totalmax
 
-            # return the longest one between left_path and right_path;
-            # remember to add 1 for the path connecting the node and its parent
-            return max(left_path, right_path) + 1
+    def maxdepth(self, root):
+        if root == None:
+            return 0
+        leftmax = self.maxdepth(root.left)
+        rightmax = self.maxdepth(root.right)
+        self.totalmax = max(leftmax+rightmax, self.totalmax)
+        return 1+max(leftmax,rightmax)
 
-        longest_path(root)
-        return diameter
+
+a = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3))
+b = TreeNode(1, TreeNode(2))
+test = Solution()
+test.diameterOfBinaryTree(b)
+test.diameterOfBinaryTree(a)
