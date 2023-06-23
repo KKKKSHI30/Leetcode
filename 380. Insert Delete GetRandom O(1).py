a98f35c88f8943b59965891268e29cbe3e08bce0
KKKKSHI <98501114+KKKKSHI30@users.noreply.github.com>
2023-06-22 21:26:53 -0400

    Binary Search + Greedy

--- 380. Insert Delete GetRandom O(1).py
+++ 380. Insert Delete GetRandom O(1).py
@@ -1,43 +1,34 @@
-from random import choice
+# HashMap + ArrayList Approach
+# Time: O(1)
+# Space: O(1)
+# 2023.06.22:no
+# notes: 对各种数据结构不够了解，思路不难，但是很难想到用hashmap+arraylist
+import random
 
-
-class RandomizedSet():
+class RandomizedSet:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.dict = {}
-        self.list = []
+        self.nums = []
+        self.valToIndex = {}
 
-    def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
-        if val in self.dict:
+    def insert(self, val):
+        if val in self.valToIndex:
             return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
+        self.valToIndex[val] = len(self.nums)
+        self.nums.append(val)
         return True
 
-    def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        if val in self.dict:
-            # move the last element to the place idx of the element to delete
-            last_element, idx = self.list[-1], self.dict[val]
-            self.list[idx], self.dict[last_element] = last_element, idx
-            # delete the last element
-            self.list.pop()
-            del self.dict[val]
-            return True
-        return False
+    def remove(self, val):
+        if val not in self.valToIndex:
+            return False
+        index = self.valToIndex[val]
+        self.valToIndex[self.nums[-1]] = index
+        self.nums[index], self.nums[-1] = self.nums[-1], self.nums[index]
+        self.nums.pop()
+        del self.valToIndex[val]
+        return True
 
-    def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
-        return choice(self.list)
+    def getRandom(self):
+        return random.choice(self.nums)
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
--- 870. Advantage Shuffle.py
+++ 870. Advantage Shuffle.py
@@ -1,57 +1,25 @@
+# Greedy Approach
+# Time: O(nlogn)
+# Space: O(n)
+# 2023.06.22: yes
+# notes: 有两个点，第一，数值是可以重复的，所以一个nums2的element可以对应多个nums1，所以需要ditionary  = {1: [], 2: []}
+# 第二个点，输出的时候，可以直接根据未排序的顺序（根据序号）去输出结果，想法不难，但是细节还挺折磨人的
 class Solution(object):
-    def advantageCount(self, A, B):
-        sortedA = sorted(A)
-        sortedB = sorted(B)
-
-        # assigned[b] = list of a that are assigned to beat b
-        # remaining = list of a that are not assigned to any b
-        assigned = {b: [] for b in B}
-        remaining = []
-
-        # populate (assigned, remaining) appropriately
-        # sortedB[j] is always the smallest unassigned element in B
-        j = 0
-        for a in sortedA:
-            if a > sortedB[j]:
-                assigned[sortedB[j]].append(a)
-                j += 1
+    def advantageCount(self, nums1, nums2):
+        assigned = {num2: [] for num2 in nums2}
+        sorted_nums2 = sorted(nums2, reverse = True)
+        sorted_nums1 = sorted(nums1, reverse = True)
+        for j in range(len(sorted_nums2)):
+            if sorted_nums1[0] > sorted_nums2[j]:
+                assigned[sorted_nums2[j]].append(sorted_nums1.pop(0))
             else:
-                remaining.append(a)
+                assigned[sorted_nums2[j]].append(sorted_nums1.pop(-1))
+            j += 1
+        return [assigned[b].pop() for b in nums2]
 
-        # Reconstruct the answer from annotations (assigned, remaining)
-        return [assigned[b].pop() if assigned[b] else remaining.pop()
-                for b in B]
-
+# Tests:
 test = Solution()
+test.advantageCount([2,0,4,1,2],[1,3,0,0,2])
 test.advantageCount([12,24,8,32],[13,25,32,11])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
