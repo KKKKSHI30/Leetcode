912935eb98e143f915df77ac2eb8c033dd159362
KKKKSHI <98501114+KKKKSHI30@users.noreply.github.com>
2023-07-02 21:22:52 -0400

    binary tree

---
+++ 103. Binary Tree Zigzag Level Order Traversal.py
@@ -0,0 +1,10 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# breadth-first Approach
+# Time: O(n)
+# Space: O(n)
+# 2023.07.02: yes
